441bd36c14ba33199fce7a75a64b71cf7913c920
jv-vogler <jvsvogler@gmail.com>
2024-06-15 14:58:13 -0300

    batman: this commit has no parents

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="container m-auto"></div>
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'automata-black': ['AutomataBlack', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
